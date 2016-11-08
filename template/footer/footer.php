8946d7214950822b15c0adde1234ed775329bde9
Rob Hendrikx <robhendrikx94@hotmail.com>
2016-11-08 12:27:55 +0100

    Js/css mappen en links v2

--- template/footer/footer.php
+++ template/footer/footer.php
@@ -204,16 +204,16 @@
 <!-- ./wrapper -->
 
 <!-- jQuery 2.2.3 -->
-<script src="../../js/jquery-2.2.3.min.js"></script>
+<script src="js/jquery-2.2.3.min.js"></script>
 <!-- Bootstrap 3.3.6 -->
-<script src="../../js/bootstrap.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 <!-- SlimScroll -->
-<script src="../../js/jquery.slimscroll.min.js"></script>
+<script src="js/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
-<script src="../../js/fastclick.js"></script>
+<script src="js/fastclick.js"></script>
 <!-- AdminLTE App -->
-<script src="../../js/app.min.js"></script>
+<script src="js/app.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="../../js/demo.js"></script>
+<script src="js/demo.js"></script>
 </body>
 </html>
