d8e12a5729d8677d7f70c9d1dccf69c0ef93a71c
Dirk Verkoelen <d.verkoelen@outlook.com>
2016-11-08 11:13:53 +0100

    registration page
    begin gemaakt aan registratie pagina

---
+++ pages/registration.php
@@ -0,0 +1,53 @@
+<?php
+/*
+require 'php/bootstrap.php';
+$a = new MainClass();
+$a->header();
+*/
+?>
+
+<div class="container">
+  <div class="row">
+      <form class="form-horizontal" role="form">
+          <div class="form-group">
+              <div class="col-sm-9">
+                <span class="glyphicons glyphicons-user"></span>
+                <input type="text" class="form-control" name="first_name" id="first_name"  placeholder="Voornaam"/>
+              </div>
+              <div class="col-sm-9">
+                <span class="glyphicons glyphicons-group"></span>
+                <input type="text" class="form-control" name="last_name" id="last_name"  placeholder="Achternaam"/>
+              </div>
+              <div class="col-sm-9">
+                <span class="glyphicons glyphicons-envelope"></span>
+                <input type="text" class="form-control" name="email" id="email"  placeholder="e-mail"/>
+              </div>
+              <div class="col-sm-9">
+                <span class="glyphicons glyphicons-gender-ori-hetero"></span>
+                <input type="text" class="form-control" name="gender" id="gender"  placeholder="Geslacht"/>
+              </div>
+              <div class="col-sm-9">
+                <span class="glyphicons glyphicons-calendar"></span>
+                <input type="text" class="form-control" name="birthday" id="birthday"  placeholder="Geboortedatum"/>
+              </div>
+              <div class="col-sm-9">
+                <span class="glyphicons glyphicons-book-open"></span>
+                <input type="text" class="form-control" name="biography" id="biography"  placeholder="Biografie"/>
+              </div>
+              <div class="col-sm-9">
+                <span class="glyphicons glyphicons-settings"></span>
+                <input type="text" class="form-control" name="skills" id="skills"  placeholder="Vaardigheden"/>
+              </div>
+              <div class="col-sm-9">
+                <span class="glyphicons glyphicons-home"></span>
+                <input type="text" class="form-control" name="street" id="street"  placeholder="Straat"/>
+                <input type="text" class="form-control" name="house_number" id="house_number"  placeholder="Huisnummer"/>
+              </div>
+          </div>
+        </form> <!-- /form -->
+    </div>
+  </div>
+</div>
+
+
+<?php //$a->footer(); ?>
